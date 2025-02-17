421012e4aa0fe0cbde72735557ffdcc0ae2404d5
kelvin389 <kelvinl.leung@gmail.com>
2025-02-16 18:58:36 -0800

    volume macro

--- keyboards/keychron/v3_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v3_max/ansi_encoder/keymaps/via/keymap.c
@@ -59,6 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,         KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,         KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,         VOLMACRO,    KC_PSCR,  KC_CTANA, RGB_MOD,
         KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                  KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                  KC_RSFT,              KC_UP,
         KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN),  MO(WIN_FN2), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
@@ -118,6 +119,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SOCDTOG:
             if (record->event.pressed) {
                 socd_cleaner_enabled = !socd_cleaner_enabled;
+            }
+            return false;
+        case VOLMACRO:
+            if (record->event.pressed) {
+                // volume down 50 times to mute
+                for (uint8_t i = 0; i < 50; i++) {
+                    tap_code(KC_VOLD);
+                }
+                // volume up 6 times = 12 volume
+                for (uint8_t i = 0; i < 6; i++) {
+                    tap_code(KC_VOLU);
+                }
             }
             return false;
     }
